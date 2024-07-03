1de32942d65d57d00c4f5ca77b71526b459c0e80
Camilo Ávila <98609727+8ctal@users.noreply.github.com>
2024-07-03 16:43:58 -0500

    "Create folder includes, rename .html files to .php and added bootstrap 5.3.3"

---
+++ models/index.php
@@ -0,0 +1,26 @@
+<?php include("C:\xampp\htdocs\DB_project\conecction.php") ?>
+<?php include ("../includes/header.php") ?>
+
+    <div class="container">
+        <div class="sidebar">
+            <ul class="menu">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+        <div class="content">
+            <h1>Welcome to the Modern Page!</h1>
+            <p>This is the main content area of your page.</p>
+        </div>
+    </div>
+    <button id="btn">Click Me</button>
+
+    <!-- SCRIPTS -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="scripts.js"></script>
+</body>
+
+</html>
